eb08eb08bd73af63382e0d5eaf47bf5d95dd4397
John Joyce <jjoyce6@nd.edu>
2017-03-19 22:31:31 -0400

    added some stuff

--- GoServer/server.go
+++ GoServer/server.go
@@ -8,13 +8,91 @@ import (
   "io/ioutil"
   "bytes"
 	"strings"
+
 	//"net/http/httputil"
 )
+
+
+//************** IRON.IO STUFF *******************//
+
+type Task struct {
+        CodeName string `json:"code_name"`
+        Payload string `json:"payload"`
+        StartAt string `json:"start_at"`
+}
+
+type ReqData struct {
+        Tasks []*Task `json:"tasks"`
+}
+
+func queueTwilio(execTime string, msg string) string {
+        const token = "IpsW3ZNlFsLl42T2vSqw"
+        const project = "58cf2ffd0e2c7300061ad812"
+
+        // Insert our project ID and token into the API endpoint
+        target := fmt.Sprintf("http://worker-us-east.iron.io/2/projects/%s/schedules?oauth=%s", project, token)
+
+        // Build the payload
+        // The payload is a string to pass information into your worker as part of a task
+        // It generally is a JSON-serialized string (which is what we're doing here) that can be deserialized in the worker
+        payload := map[string]interface{} {
+                "Message" : "Test",
+        }
+
+        payload_bytes, err := json.Marshal(payload)
+        if err != nil {
+                panic(err.Error())
+        }
+        payload_str := string(payload_bytes)
+
+        // Build the task
+        task := &Task {
+                CodeName: "irontest",
+                Payload: payload_str,
+                StartAt: execTime,
+        }
+
+        // Build a request containing the task
+        json_data := &ReqData {
+                Tasks: []*Task { task },
+        }
+
+        json_bytes, err := json.Marshal(json_data)
+
+        if err != nil {
+                panic(err.Error())
+        }
+
+        json_str := string(json_bytes)
+
+        // Post expects a Reader
+        json_buf := bytes.NewBufferString(json_str)
+
+        // Make the request
+        resp, err := http.Post(target, "application/json", json_buf)
+        if err != nil {
+                panic(err.Error())
+        }
+        defer resp.Body.Close()
+
+        // Read the response
+        resp_body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+                panic(err.Error())
+        }
+
+        // Print the response to STDOUT
+        fmt.Println(string(resp_body))
+
+        return "Okay. I will send you a text message to remind you!"
+}
+
 
 type WebhookResult struct {
 	Action string
   Parameters WebhookParameters 
   Fulfillment WebhookFulfillment
+  ResolvedQuery string 
 }
 
 type WebhookParameters struct {
@@ -308,10 +386,9 @@ func webhook_handler(rw http.ResponseWriter, request* http.Request) {
       response = getCurrentWeather(t.Result.Parameters.City)
     case "current_time": 
       //get the current time 
-    case "alarm":
-      //set an alarm somehow 
     case "reminder":
       //add a reminder 
+      response = queueTwilio(t.Result.Parameters.DateTime, t.Result.ResolvedQuery)
     case "netflix": 
       //check if its on netflix 
     default:
